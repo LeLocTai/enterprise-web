500c49fb4b35d799f0bb7f61d4ef1f87a177be23
Unknown <vzttzv@gmail.com>
2019-03-04 12:08:35 +0700

    WIP: repo

---
+++ Repository/DatabaseManager.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    static final String DB_URL = "jdbc:mariadb://ewd.c6d1svw4itgu.ap-southeast-1.rds.amazonaws.com/magazinedb";
+    static final String DB_URL_LOCAL = "jdbc:mariadb://localhost/magazinedb";
+
+    //  Database credentials
+    static final String USER = "ewd";
+    static final String PASS = "ewd12345";
+
+    public DatabaseManager() {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL_LOCAL, "root", "");
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
