54ffd810d6a8de3cb415d1a4e6175e2d051c4cd0
Tai Le <vzttzv@gmail.com>
2019-03-04 10:42:48 +0700

    create getter setter contstructor for models

--- Model/Faculties.java
+++ Model/Faculties.java
@@ -4,4 +4,37 @@ public class Faculties {
 	private int _id;
 	private String _name;
 	public Vector<Users> _users = new Vector<Users>();
+
+	public Faculties() {
+	}
+
+	public Faculties(int _id, String _name, Vector<Users> _users) {
+		this._id = _id;
+		this._name = _name;
+		this._users = _users;
+	}
+
+	public int get_id() {
+		return _id;
+	}
+
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+
+	public String get_name() {
+		return _name;
+	}
+
+	public void set_name(String _name) {
+		this._name = _name;
+	}
+
+	public Vector<Users> get_users() {
+		return _users;
+	}
+
+	public void set_users(Vector<Users> _users) {
+		this._users = _users;
+	}
 }
--- Model/Users.java
+++ Model/Users.java
@@ -7,4 +7,64 @@ public class Users {
 	private String _email;
 	public Faculties _faculty;
 	public Vector<Submissions> _submissions = new Vector<Submissions>();
+
+	public int get_id() {
+		return _id;
+	}
+
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+
+	public String get_role() {
+		return _role;
+	}
+
+	public void set_role(String _role) {
+		this._role = _role;
+	}
+
+	public boolean is_has_Accepted_TOC() {
+		return _has_Accepted_TOC;
+	}
+
+	public void set_has_Accepted_TOC(boolean _has_Accepted_TOC) {
+		this._has_Accepted_TOC = _has_Accepted_TOC;
+	}
+
+	public String get_email() {
+		return _email;
+	}
+
+	public void set_email(String _email) {
+		this._email = _email;
+	}
+
+	public Faculties get_faculty() {
+		return _faculty;
+	}
+
+	public void set_faculty(Faculties _faculty) {
+		this._faculty = _faculty;
+	}
+
+	public Vector<Submissions> get_submissions() {
+		return _submissions;
+	}
+
+	public void set_submissions(Vector<Submissions> _submissions) {
+		this._submissions = _submissions;
+	}
+
+	public Users(int _id, String _role, boolean _has_Accepted_TOC, String _email, Faculties _faculty, Vector<Submissions> _submissions) {
+		this._id = _id;
+		this._role = _role;
+		this._has_Accepted_TOC = _has_Accepted_TOC;
+		this._email = _email;
+		this._faculty = _faculty;
+		this._submissions = _submissions;
+	}
+
+	public Users() {
+	}
 }
