a28aaa33999c673b2443153b2f836c4c8723bded
Tai Le <vzttzv@gmail.com>
2019-03-20 15:26:07 +0700

    submission download servlet

---
+++ src/com/magazineapp/service/SubmissionDownloadServlet.java
@@ -0,0 +1,69 @@
+package com.magazineapp.service;
+
+import com.magazineapp.model.Submission;
+import com.magazineapp.model.User;
+import com.magazineapp.repository.SubmissionRepo;
+
+import javax.mail.Session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet("/submission-download")
+public class SubmissionDownloadServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException
+    {
+        int        submissionId = Integer.parseInt(request.getParameter("submission-id"));
+        Submission submission   = new SubmissionRepo().get(submissionId);
+        if (submission == null) return;
+
+        User authUser = (User) request.getSession().getAttribute("user");
+        if (!isAuthorized(submission, authUser))
+        {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+
+        File file = new File(submission.get_path());
+
+        response.setContentType("application/force-download");
+        response.setContentLength((int) file.length());
+        response.setHeader("Content-Transfer-Encoding", "binary");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+
+        copyBuffered(
+                new FileInputStream(file),
+                response.getOutputStream(),
+                new byte[1024 * 4]
+        );
+    }
+
+    private boolean isAuthorized(Submission submission, User authUser)
+    {
+        if (authUser.get_role().equalsIgnoreCase("manager") ||
+            authUser.get_role().equalsIgnoreCase("coordinator"))
+            return true;
+
+        if (submission.get_author().get_id() == authUser.get_id())
+            return true;
+
+        return false;
+    }
+
+    private void copyBuffered(InputStream from, OutputStream to, byte[] buffer) throws IOException
+    {
+        int nextLength;
+        while ((nextLength = from.read(buffer)) > 0)
+        {
+            to.write(buffer, 0, nextLength);
+        }
+
+        from.close();
+        to.flush();
+    }
+}
