9aa36d1d303896b5f55a5a2bb1d18963083fff81
Tai Le <vzttzv@gmail.com>
2019-03-25 11:03:40 +0700

    auto redirect to previous page after login

--- src/com/magazineapp/filter/AuthFilter.java
+++ src/com/magazineapp/filter/AuthFilter.java
@@ -10,36 +10,34 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "authentication")
-public class AuthFilter implements Filter
-{
+public class AuthFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException
-    {
+    public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException
-    {
-        HttpServletRequest  request  = (HttpServletRequest) servletRequest;
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpSession         session  = request.getSession(false); //dont auto create
-        String              loginURI = request.getContextPath() + "/login.jsp";
+        HttpSession session = request.getSession(false); //dont auto create
+        String loginURI = request.getContextPath() + "/login.jsp";
 
-        boolean isLoggedIn      = session != null && session.getAttribute("user") != null;
+        boolean isLoggedIn = session != null && session.getAttribute("user") != null;
         boolean isTryingToLogin = request.getRequestURI().equals(loginURI);
 
         if (isLoggedIn || isTryingToLogin)
             filterChain.doFilter(request, response);
-        else
-            response.sendRedirect(loginURI);
+        else {
+            response.setHeader("Redirect", request.getRequestURL().toString());
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 
     @Override
-    public void destroy()
-    {
+    public void destroy() {
 
     }
 }
