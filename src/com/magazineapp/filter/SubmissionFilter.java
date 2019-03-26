634ec3c586457ebce44a44845ef5fe879d652505
Tai Le <vzttzv@gmail.com>
2019-03-26 19:32:09 +0700

    fix login redirect

--- src/com/magazineapp/filter/SubmissionFilter.java
+++ src/com/magazineapp/filter/SubmissionFilter.java
@@ -10,37 +10,41 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "submission")
-public class SubmissionFilter implements Filter {
+public class SubmissionFilter implements Filter
+{
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
 
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+                         FilterChain filterChain) throws IOException, ServletException
+    {
+        HttpServletRequest  request  = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpSession session = request.getSession();
+        HttpSession         session  = request.getSession();
 
         User user = (User) session.getAttribute("user");
 
-        if (user == null) {
-            response.sendRedirect(request.getContextPath() + "/login.jsp");
-        } else if (user.get_role().equalsIgnoreCase("student") ||
-                   user.get_role().equalsIgnoreCase("coordinator")) {
+        if (user.get_role().equalsIgnoreCase("student") ||
+            user.get_role().equalsIgnoreCase("coordinator"))
+        {
             if (!user.get_has_Accepted_TOC())
                 response.sendRedirect(request.getContextPath() + "/tos.jsp");
             else
                 filterChain.doFilter(request, response);
-        } else {
+        } else
+        {
             response.sendError(HttpServletResponse.SC_FORBIDDEN);
         }
     }
 
     @Override
-    public void destroy() {
+    public void destroy()
+    {
 
     }
 }
