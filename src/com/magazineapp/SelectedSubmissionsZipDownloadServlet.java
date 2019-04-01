eb13c475d64e99a39a5769b6b65f12bcff0429ce
Tai Le <vzttzv@gmail.com>
2019-04-01 17:26:42 +0700

    zip download servlet and service

--- src/com/magazineapp/SelectedSubmissionsZipDownloadServlet.java
+++ src/com/magazineapp/SelectedSubmissionsZipDownloadServlet.java
@@ -2,6 +2,7 @@ package com.magazineapp;
 
 import com.magazineapp.model.Submission;
 import com.magazineapp.repository.SubmissionRepo;
+import com.magazineapp.service.HttpResponseService;
 import com.magazineapp.service.ZipService;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,15 +18,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/download-selected-as-zip")
-public class SelectedSubmissionsZipDownloadServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Submission> selectedSubmission = new SubmissionRepo().getSelected();
-        List<Path> selectedSubmissionPaths = new ArrayList<>();
+public class SelectedSubmissionsZipDownloadServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        List<Submission> selectedSubmission      = new SubmissionRepo().getSelected();
+        List<Path>       selectedSubmissionPaths = new ArrayList<>();
 
-        for (Submission curSubmission : selectedSubmission) {
+        for (Submission curSubmission : selectedSubmission)
+        {
             selectedSubmissionPaths.add(Paths.get(curSubmission.get_path()));
         }
 
-        ZipService.createFromPaths(selectedSubmissionPaths);
+        File zipFile = ZipService.createFromPaths(selectedSubmissionPaths);
+
+        HttpResponseService.ForceDownload(response, zipFile);
     }
 }
--- src/com/magazineapp/service/ZipService.java
+++ src/com/magazineapp/service/ZipService.java
@@ -1,32 +1,39 @@
 package com.magazineapp.service;
 
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.*;
-import java.util.HashMap;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-public class ZipService {
-    public static Path createFromPaths(List<Path> inPaths) throws IOException {
-        Path outPath = FileSystemService.getZipFilePath();        
+public class ZipService
+{
+    public static File createFromPaths(List<Path> inPathStrings) throws IOException
+    {
+        File outFile = FileSystemService.getZipFilePath().toFile();
 
-        Map<String, String> env = new HashMap<>();
-        env.put("create", "true");//create zip if not exists, prob not necessary
+        FileOutputStream     fileOutStream     = new FileOutputStream(outFile);
+        BufferedOutputStream bufferedOutStream = new BufferedOutputStream(fileOutStream);
 
-        //using .zip extension should automatically use zipfs provider
-        try (FileSystem zipfs = FileSystems.newFileSystem(outPath.toUri(), env)) {
-            //Just to know if environment lack provider
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info(zipfs.provider().toString());
-            
-            for (Path curPath : inPaths) {
-                Path pathInZipfile = zipfs.getPath(curPath.getFileName().toString());
+        //close is propagated
+        try (ZipOutputStream zipOutStream = new ZipOutputStream(bufferedOutStream, StandardCharsets.UTF_8))
+        {
+            for (Path curPath : inPathStrings)
+            {
+                File curFile = curPath.toFile();
+                try (FileInputStream curFileInStream = new FileInputStream(curFile))
+                {
+                    ZipEntry zipEntry = new ZipEntry(curFile.getName());
+                    zipOutStream.putNextEntry(zipEntry);
 
-                Files.copy(curPath, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
+                    FileSystemService.transferStreamsBuffered(curFileInStream, zipOutStream);
+
+                    zipOutStream.closeEntry();
+                }
             }
         }
-        
-        return outPath;
+
+        return outFile;
     }
 }
