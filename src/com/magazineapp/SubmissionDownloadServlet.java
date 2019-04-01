68ec72555a196468ca1087d1206db562e086ea02
Tai Le <vzttzv@gmail.com>
2019-04-01 17:26:30 +0700

    use new service in submisison download

--- src/com/magazineapp/SubmissionDownloadServlet.java
+++ src/com/magazineapp/SubmissionDownloadServlet.java
@@ -3,7 +3,7 @@ package com.magazineapp;
 import com.magazineapp.model.Submission;
 import com.magazineapp.model.User;
 import com.magazineapp.repository.SubmissionRepo;
-import com.magazineapp.service.FileSystemService;
+import com.magazineapp.service.HttpResponseService;
 import org.apache.commons.lang.math.NumberUtils;
 
 import javax.servlet.ServletException;
@@ -37,21 +37,9 @@ public class SubmissionDownloadServlet extends HttpServlet
             return;
         }
 
-        File file = new File(submission.get_path());
+        File submissionFile = new File(submission.get_path());
 
-        response.setContentType("application/force-download");
-        response.setContentLength((int) file.length());
-        response.setHeader("Content-Transfer-Encoding", "binary");
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
-
-        try (FileInputStream submissionFileInStream = new FileInputStream(file))
-        {
-            FileSystemService.transferStreamsBuffered(
-                    submissionFileInStream,
-                    response.getOutputStream(),
-                    1024 * 4
-            );
-        }
+        HttpResponseService.ForceDownload(response, submissionFile);
     }
 
     private boolean isAuthorized(Submission submission, User authUser)
